91ee42d158a4b5201e65bdd33a5fbcc676679553
Mónica Espiñeira <monica.espineira1988@gmail.com>
2025-10-08 18:40:06 +0200

    ejercicio uno

---
+++ 1.Programación/VariablesEJ1/src/Variablesej1.java
@@ -0,0 +1,13 @@
+public class Variablesej1 {
+
+    public static void main(String [] args){
+        String nombreCompleto = " Mónica Espiñeira Aragón";
+        int edad = 37;
+        String ciudad = "Vigo";
+
+        System.out.println("Mi nombre es"+nombreCompleto);
+        System.out.println("Tengo" +edad);
+        System.out.println("He nacido en la ciudad de "+ciudad);
+
+    }
+}
