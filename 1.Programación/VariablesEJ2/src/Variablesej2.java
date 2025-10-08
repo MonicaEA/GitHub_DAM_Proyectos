605f9a7ca145a0004b7c86fa9e7f10c205a04a06
Mónica Espiñeira <monica.espineira1988@gmail.com>
2025-10-08 19:00:54 +0200

    subidos nuevos ejercicios
    he finalizado ejercicios 2 y 3

---
+++ 1.Programación/VariablesEJ2/src/Variablesej2.java
@@ -0,0 +1,19 @@
+public class Variablesej2 {
+
+    public static void main(String[] args) {
+        int numero = 0;
+
+        System.out.println(numero);
+
+        numero=numero+5;
+        System.out.println(numero);
+
+        numero=numero+5;
+        System.out.println(numero);
+
+        numero=numero+5;
+        System.out.println(numero);
+
+
+    }
+}
---
+++ 1.Programación/VariajesEJ3/src/Variablesej3.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Variablesej3 {
+    public static void main(String[] args) {
+
+        String nombre = "Mónica";
+        int edad = 37;
+        boolean estudiante= true;
+        double altura= 1.60;
+        char inicial= 'M';
+
+        System.out.println("Nombre del candidato: "+nombre);
+        System.out.println("Edad: "+edad+" años");
+        System.out.println("¿Es estudiante? "+estudiante);
+        System.out.println("Altura: "+altura+"cm");
+        System.out.println("Inicial "+inicial);
+
+
+    }
+}
