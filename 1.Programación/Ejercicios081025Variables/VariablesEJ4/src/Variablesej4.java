89b1b6fe76103bb72f2190a19d4d4afeda28b802
Mónica Espiñeira <monica.espineira1988@gmail.com>
2025-10-09 11:54:12 +0200

    ejercicios variables
    subidos finalizados los 5 ejercicios

---
+++ 1.Programación/Ejercicios081025Variables/VariablesEJ4/src/Variablesej4.java
@@ -0,0 +1,21 @@
+public class Variablesej4 {
+    public static void main(String[] args) {
+
+        String titulo ="Las frases frágiles ";
+        String autor="Emilia Pardo Bazán ";
+        int publicacion=2023;
+        int paginas=107;
+        boolean disponible = true;
+
+        System.out.println("Título: "+titulo);
+        System.out.println("Autor/a: "+autor);
+        System.out.println("Año de publicación: "+publicacion);
+        System.out.println("Número de páginas: "+paginas);
+        System.out.println("¿Está disponible? "+disponible);
+
+
+
+    }
+
+
+}
---
+++ 1.Programación/Ejercicios081025Variables/VariablesEJ5/src/Variablesej5.java
@@ -0,0 +1,31 @@
+public class Variablesej5 {
+    public static void main(String[] args) {
+
+        final String aplicacion="MiApp";
+        String version="1.0.0";
+        final double pi=3.14159;
+        String usuario="Laura";
+        int nivel=1;
+        int puntuacion=0;
+
+        System.out.println("Aplicación: "+aplicacion);
+        System.out.println("Versión: "+version);
+        System.out.println("Valor de PI: "+pi);
+        System.out.println("Usuario actual: "+usuario);
+        System.out.println("Nivel:"+nivel);
+        System.out.println("Puntuación:"+puntuacion);
+
+        usuario="Miguel";
+        nivel=2;
+        puntuacion=150;
+
+
+        System.out.println("Usuario actualizado: "+usuario);
+        System.out.println("Nivel actualizado:"+nivel);
+        System.out.println("Puntuación actualizada:"+puntuacion);
+
+
+
+
+    }
+}
