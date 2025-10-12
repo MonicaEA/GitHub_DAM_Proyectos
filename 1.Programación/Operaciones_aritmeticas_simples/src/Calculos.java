2416e0ce7819c65a733d0284db8fe81665e67a80
Mónica Espiñeira <monica.espineira1988@gmail.com>
2025-10-12 18:59:22 +0200

    primer ejercicio

---
+++ 1.Programación/Operaciones_aritmeticas_simples/src/Calculos.java
@@ -0,0 +1,34 @@
+public class Calculos {
+
+int num1=10;
+        int num2 =5;
+
+
+    public void sumar (){
+int resultado= num1+num2;
+        System.out.println("El resultado de la suma es "+resultado);
+
+    }
+
+    public void restar (){
+        int resultado = num1-num2;
+        System.out.println("El resultado de la resta es "+resultado);
+
+
+    }
+
+
+    public void multiplicacion (){
+        int resultado = num1*num2;
+        System.out.println("El resultado de la multiplicación es "+resultado);
+
+
+    }
+
+    public void division (){
+        int resultado = num1/num2;
+        System.out.println("División: "+resultado);
+
+
+    }
+}
--- 1.Programación/Operaciones_aritmeticas_simples/src/Operaciones.java
+++ 1.Programación/Operaciones_aritmeticas_simples/src/Operaciones.java
@@ -1,6 +1,21 @@
 public class Operaciones {
 
     public static void main(String[] args) {
+        System.out.println("Te damos la bienvenida a nuestra calculadora ");
+        System.out.println("Vamos a realizar una serie de operaciones sencillas ");
+        System.out.println();
+
+
+        Calculos operaciones = new Calculos();
+        System.out.println("El primer número es "+ operaciones.num1);
+        System.out.println("El segundo número es "+ operaciones.num2);
+        operaciones.sumar();
+        operaciones.restar();
+        operaciones.multiplicacion();
+        operaciones.division();
+
+
+
 
 
     }
